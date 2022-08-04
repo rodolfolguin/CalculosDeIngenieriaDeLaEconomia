211b930ddcdf3a4c77640f0a0d3ecbf9e3f476a9
Rodolfo Olguin <rodolfolguin2@gmail.com>
2022-08-04 15:02:09 -0400

    Se crea valor futuro, anualidad valor futuro, gradiente

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function calcularvp() {
+function calcularVp() /* Calculo valor presente */
+{
 
     var a1 = parseFloat(document.getElementById("a1").value);
     var ti1 = parseFloat(document.getElementById("ti1").value);
@@ -16,7 +17,8 @@ function calcularvp() {
     document.getElementById('res1').innerHTML = currencyString;
 }
 
-function calcularan() {
+function calcularAnVp()/* Calculo anualidad valor presente */
+{
 
     var vp2 = parseFloat(document.getElementById("vp2").value);
     var ti2 = parseFloat(document.getElementById("ti2").value);
@@ -32,4 +34,67 @@ function calcularan() {
     var currencyString = myNumeral.format('$0,0.00');
 
     document.getElementById('res2').innerHTML = currencyString;
+}
+
+function calcularVf()/* Calculo valor futuro*/
+{
+
+    var a3 = parseFloat(document.getElementById("a3").value);
+    var ti3 = parseFloat(document.getElementById("ti3").value);
+    var p3 = parseFloat(document.getElementById("p3").value);
+
+
+    var vc3 = 1 + ti3;
+    var ele3 = Math.pow(vc3, p3);
+    var pre3 = a3 * ((ele3 - 1) / ti3);
+
+    var number = pre3;
+    var myNumeral = numeral (number);
+    var currencyString = myNumeral.format('$0,0.00');
+
+    document.getElementById('res3').innerHTML = currencyString;
+}
+
+function calcularAnVf()/* Calculo anualidad valor futuro */
+{
+
+    var vf4 = parseFloat(document.getElementById("vf4").value);
+    var ti4 = parseFloat(document.getElementById("ti4").value);
+    var p4 = parseFloat(document.getElementById("p4").value);
+
+
+    var vc4 = 1 + ti4;
+    var ele4 = Math.pow(vc4, p4);
+    var pre4 = vf4 * (ti4 / (ele4 - 1) );
+
+    var number = pre4;
+    var myNumeral = numeral (number);
+    var currencyString = myNumeral.format('$0,0.00');
+
+    document.getElementById('res4').innerHTML = currencyString;
+}
+
+function calcularG() /* Calculo valor presente */
+{
+
+    var a5 = parseFloat(document.getElementById("a5").value);
+    var ti5 = parseFloat(document.getElementById("ti5").value);
+    var p5 = parseFloat(document.getElementById("p5").value);
+    var g5 =parseFloat(document.getElementById("g5").value);
+
+
+    var vc5 = 1 + ti5;
+    var ele5 = Math.pow(vc5, p5);
+
+    var g = g5/ti5;
+    var g2 = ((ele5-1)/ti5)-p5;
+    var g3 = 1/ele5;
+
+    var pre5 = g*g2*g3;
+
+    var number = pre5;
+    var myNumeral = numeral (number);
+    var currencyString = myNumeral.format('$0,0.00');
+
+    document.getElementById('res5').innerHTML = currencyString;
 }
